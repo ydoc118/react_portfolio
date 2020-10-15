99b7b032db1e1f51e0f4c02f4eb7fa912311924d
Cody <ydoc118@gmail.com>
2020-10-15 18:13:22 -0400

    changed name

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
             <header>
                 <figure className="jumbotron jumbotron-fluid homeJumbo">
                     <div className="container">
-                    <h1 className="display-4">Cody Blankenship</h1>
+                    <h1 className="display-4">Blankenship</h1>
                     <p className="lead">Welcome to my Professional Portfolio</p>
                     </div>
                 </figure>
